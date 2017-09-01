131f3ff7825cae0c4f95a96fee902dccd811ad54
Darren-Haynes <darrenhaynes@zoho.com>
2017-09-01 06:52:09 -0700

    added documentation

--- mount_usb.sh
+++ mount_usb.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+# get UUID of plugged in usb drive. Note fails if more than 1 USB drive is plugged in
 UUID=$(lsblk -f | grep sdb | grep -oE '\b[a-zA-Z0-9-]{6,}\b')
+
+# Directory where usb drive is mounted
 DIR='/dev/usb/'
-if [ !  -d "$DIR" ]; then
+
+# create directory where usb is mounted if it doesn't exist
+if [ ! -d "$DIR" ]; then
   sudo mkdir /dev/usb/
 fi
+
+# mount
 sudo mount -U "$UUID" "$DIR"
