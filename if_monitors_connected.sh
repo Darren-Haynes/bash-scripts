28b8676be09c2c6c590fdcca8da772b26bca14ac
Darren-Haynes <darrenhaynes@zoho.com>
2017-09-03 22:52:14 -0700

    added documentation

--- if_monitors_connected.sh
+++ if_monitors_connected.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# This scripts checks if my external monitors are connected by querying
+# xrandr. If 3 screens are connected (laptop plus 2 external monitors) then
+# run my other script that makes the xrandr magic happen to set up the 3 screens.
+# Note this script is run automatically from i3wm config file on execution of
+# the i3wm.
+
 screens_connected=$(xrandr | grep -cw connected)
 
 if [[ $screens_connected -eq 3 ]]; then
   sh /home/darren/.bash_scripts/3screen.sh
+  echo "3 screens scripts has been executed"
+else
+  echo "3 screens scripts not executed. Laptop only mode"
 fi
