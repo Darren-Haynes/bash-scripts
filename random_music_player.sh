0bb744fd1df97612b03489c2cb5e092db7307ec9
Darren-Haynes <darrenhaynes@zoho.com>
2017-09-09 15:47:17 -0700

    initial commit of this file

---
+++ random_music_player.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get list of all .mp3 files recursively in Music directory randomly
+# then play them with mplayer
+OIFS="$IFS"
+IFS=$'\n'
+for file in `find $HOME/Music -type f -name "*.mp3" | shuf`
+do
+     mplayer $file
+done
+IFS="$OIFS"
